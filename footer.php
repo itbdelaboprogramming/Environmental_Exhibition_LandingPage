40f5a2533c51297c04a216e2104af0b5e363c221
fajarmnrozaki <zakifajar96@gmail.com>
2025-05-09 11:37:55 +0700

    add the information to footer and resize the size of header + footer

--- footer.php
+++ footer.php
@@ -1,7 +1,33 @@
-<footer id="GlobalFooter">
+<!-- <footer id="GlobalFooter">
   <div class="Foot">
     <div class="Foot">
     </div>
+  </div>
+</footer>
+</body>
+
+</html> -->
+
+<footer>
+  <div class="company-logos">
+    <!-- <a href="https://nakayamairon.com/vsi_expo_jakarta_2023/"> -->
+    <a>
+      <span><img src="./img/Nakayama logo_red_500x100.png" /></span>
+    </a>
+  </div>
+  <div class="company">
+    <p>Nakayama Iron Works, Ltd.</p>
+  </div>
+  <div class="address">
+    <p>Asahimachi, Takeo City, Saga Pref 843-0001, Japan</p>
+  </div>
+  <div class="contact-us">
+    <a href="https://www.ncjpn.com/en/contact/" target="_blank">
+      <p>Contact Us</p>
+    </a>
+  </div>
+  <div class="copyright">
+    <p>Copyright@2025 - Nakayama Iron Works, Ltd. All rights reserved</p>
   </div>
 </footer>
 </body>
